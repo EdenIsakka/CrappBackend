9e9d8d5856ac121028675c842fe0fb4d11d379d7
Maxtro2002 <jspr200231@gmail.com>
2022-05-09 14:55:44 -0500

    Perfil ticked, controller y repository agregado

--- src/main/java/com/example/crappBackend/Controller/UserController.java
+++ src/main/java/com/example/crappBackend/Controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.DELETE,RequestMethod.PUT})
 @RequestMapping("/api/user")
 public class UserController {
     @Autowired
